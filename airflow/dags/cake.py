255b7beed13eeb8f04785d3d5731a491c0b7f958
Alejandro Socarras <apsocarras@gmail.com>
2023-02-03 11:59:40 -0500

    WIP: Write first dag

--- airflow/dags/cake.py
+++ airflow/dags/cake.py
@@ -1,12 +1,36 @@
+import os 
 from datetime import datetime
 from airflow import DAG
 from airflow.decorators import dag, task
+from airflow.sensors.filesystem import FileSensor 
+from airflow.hooks.filesystem import FSHook
+
+# ## Assignment: 
+# Uses a file sensor to check whether the votes.csv file has been uploaded to the data/ folder of this repository.
+# Has a task that reads each row in votes.csv, and checks whether that value is in the flavors_choices list defined above. 
+# If it is, append it to a new list called valid_votes. This task should return the valid_votes list.
+# In another task, use a Python function that takes a list as an argument, and prints the item that appear the most times in that list.
+# Pass the return_value XCom from the first task as an argument to the second task.
+# Use decorators to define the tasks.
+
+FLAVORS = ["lemon", "vanilla", "chocolate",
+ "pistachio", "strawberry", "confetti", "caramel", "pumpkin", "rose"]
+
+FILENAME = "votes.csv"
+
+data_fs = FSHook(conn_id='data_fs')     # Make sure this airflow connection exists
+data_dir = data_fs.get_path()           # get its root path
+file_path = os.path.join(data_dir, FILENAME)
 
 
 @task
-def <func1>():
+def getVotes():
    """"""
-  return <to_return> # Passed in decorator function: t2 = func(t1)
+  data_fs = FSHook(conn_id='data_fs')     # Make sure this airflow connection exists
+  data_dir = data_fs.get_path()           # get its root path
+  file_path = os.path.join(data_dir, FILENAME)
+  
+  return to_return # Passed in decorator function: t2 = func(t1)
 
 
 @task
